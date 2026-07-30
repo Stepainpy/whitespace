8c2c19b71c46f630320e0463c0278cfd10b45540
Stepainpy <stepa2014vlg@gmail.com>
2026-07-30 22:09:06 +0300

    update include file

--- include/whitespace/whitespace.h
+++ include/whitespace/whitespace.h
@@ -11,6 +11,7 @@ typedef struct ws_state_t ws_state_t;
 
 typedef void* (ws_alloc_t)(void* ptr, size_t size, void* userdata);
 typedef size_t (ws_read_t)(void* dst, size_t size, size_t count, void* userdata);
+typedef size_t (ws_write_t)(const void* src, size_t size, size_t count, void* userdata);
 
 typedef enum {
     WSE_OK = 0,
@@ -19,12 +20,13 @@ typedef enum {
 
 const char* ws_strerror(ws_error_t error);
 
-ws_error_t ws_compile(ws_state_t** state, void* source, ws_read_t reader);
-ws_error_t ws_execute(ws_state_t*  state, void*  input, ws_read_t reader);
-
-ws_error_t wsa_compile(ws_state_t** state,
+ws_error_t ws_compile(ws_state_t** state,
     void* source, ws_read_t reader,
     ws_alloc_t allocator, void* alloc_ud);
+
+ws_error_t ws_execute(ws_state_t* state,
+    void*  input, ws_read_t  reader,
+    void* output, ws_write_t writer);
 
 #ifdef __cplusplus
 }
