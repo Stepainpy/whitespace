1744b180f91c2b76e39747ed20ee36846d98bab0
Stepainpy <stepa2014vlg@gmail.com>
2026-08-10 16:03:49 +0300

    optimize cmake script

---
+++ src/wsd.c
@@ -0,0 +1,36 @@
+#include <whitespace/whitespace.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void* ws_alloc(void* ptr, size_t size, void* ud) {
+    if (size) return realloc(ptr, size);
+    free(ptr); (void)ud; return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    ws_error_t ec = WSE_NOT_IMPL;
+    ws_code_t* code = NULL;
+    FILE* source = NULL;
+
+    --argc, ++argv; /* skip program name */
+
+    if (argc != 1) goto cleanup;
+    source = fopen(*argv, "r");
+    if (!source) goto cleanup;
+
+    ec = ws_compile(&code,
+        source, (ws_rdfn_t)fread, ws_alloc, NULL);
+    if (ec) goto cleanup;
+    fclose(source); source = NULL;
+
+    puts("==== BEGIN ====");
+    ec = ws_disasm(code, stdout, (ws_wrfn_t)fwrite);
+    if (ec) goto cleanup;
+    puts("===== END =====");
+
+cleanup:
+    if (code) ws_destroy(code);
+    if (source) fclose(source);
+    return ec;
+}
